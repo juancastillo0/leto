5aa34a6cf11066404f1c8f7255f0f0b012a807b2
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-09-05 01:06:25 +0000

    file upload test

--- leto_shelf/test/shelf_graphql_test.dart
+++ leto_shelf/test/shelf_graphql_test.dart
@@ -47,4 +47,48 @@ Future<void> main() async {
       ),
     );
   });
+
+  test('file upload', () async {
+    final operations = const GraphqlRequest(
+        query: 'mutation addFile(\$fileVar: Upload!) { '
+            ' addFileCommand (file: \$fileVar){ name mimeType sizeInBytes }'
+            ' }',
+        variables: {
+          'fileVar': null,
+        }).toJson();
+
+    final fileToUpload = http.MultipartFile.fromBytes(
+      'filename1Field',
+      utf8.encode('testString in file'),
+      filename: 'filename1.txt',
+    );
+    // TODO:
+    // final fileToUpload = http.MultipartFile.fromString(
+    //   'filename1Field',
+    //   'testString in file',
+    //   filename: 'filename1.txt',
+    // );
+
+    final request = http.MultipartRequest('POST', url)
+      ..fields['operations'] = jsonEncode(operations)
+      ..fields['map'] = jsonEncode(<String, Object?>{
+        'filename1Field': ['variables.fileVar'],
+      })
+      ..files.add(fileToUpload);
+
+    final response = await request.send();
+    expect(response.statusCode, 200);
+
+    final bodyStr = await response.stream.bytesToString();
+    final body = jsonDecode(bodyStr) as Map<String, Object?>;
+    expect(body, {
+      'data': {
+        'addFileCommand': {
+          'name': fileToUpload.filename,
+          'sizeInBytes': fileToUpload.length,
+          'mimeType': fileToUpload.contentType.mimeType,
+        }
+      }
+    });
+  });
 }
