fa1389e0a1203750e6b59aeaf6aecd1b1be7299c
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-09-05 01:40:13 +0000

    graphql server test extract models and schema

--- graphql_server/test/common.dart
+++ graphql_server/test/common.dart
@@ -1,5 +1,138 @@
+// ignore_for_file: prefer_constructors_over_static_methods
+
+import 'package:collection/collection.dart';
 import 'package:graphql_schema/graphql_schema.dart';
+import 'package:meta/meta.dart';
 import 'package:test/test.dart';
 
-final Matcher throwsAGraphQLException =
-    throwsA(predicate((Object? x) => x is GraphQLException, 'is a GraphQL exception'));
+final Matcher throwsAGraphQLException = throwsA(
+  predicate((Object? x) => x is GraphQLException, 'is a GraphQL exception'),
+);
+
+@immutable
+class Todo implements Serializable {
+  final String? text;
+  final bool? completed;
+  final DateTime time;
+  final List<User>? users;
+
+  const Todo({
+    this.text,
+    this.completed,
+    required this.time,
+    this.users,
+  });
+
+  @override
+  Map<String, Object?> toJson() {
+    return {
+      if (text != null) 'text': text,
+      if (completed != null) 'completed': completed,
+      if (users != null) 'users': users!.map((e) => e.toJson()).toList(),
+      'time': time.toIso8601String(),
+    };
+  }
+
+  static Todo fromJson(Map<String, Object?> map) {
+    return Todo(
+      text: map['text'] as String?,
+      completed: map['completed'] as bool?,
+      time: map['time'] is DateTime
+          ? map['time']! as DateTime
+          : DateTime.parse(map['time']! as String),
+      users: (map['users'] as List<Object?>?)
+          ?.map((e) => User.fromJson(e! as Map<String, Object?>))
+          .toList(),
+    );
+  }
+
+  static const serializer = SerializerFunc(fromJson: fromJson);
+
+  Todo copyWith({
+    String? text,
+    bool? completed,
+    DateTime? time,
+    List<User>? users,
+  }) {
+    return Todo(
+      text: text ?? this.text,
+      completed: completed ?? this.completed,
+      time: time ?? this.time,
+      users: users ?? this.users,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Todo(text: $text, completed: $completed,'
+        ' time: $time, users: $users)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    final listEquals = const DeepCollectionEquality().equals;
+
+    return other is Todo &&
+        other.text == text &&
+        other.completed == completed &&
+        other.time == time &&
+        listEquals(other.users, users);
+  }
+
+  @override
+  int get hashCode {
+    return text.hashCode ^ completed.hashCode ^ time.hashCode ^ users.hashCode;
+  }
+}
+
+@immutable
+class User implements Serializable {
+  final String name;
+  final int? age;
+
+  const User({
+    required this.name,
+    this.age,
+  });
+
+  User copyWith({
+    String? name,
+    int? age,
+  }) {
+    return User(
+      name: name ?? this.name,
+      age: age ?? this.age,
+    );
+  }
+
+  @override
+  Map<String, Object?> toJson() {
+    return {
+      'name': name,
+      if (age != null) 'age': age,
+    };
+  }
+
+  static User fromJson(Map<String, Object?> map) {
+    return User(
+      name: map['name']! as String,
+      age: map['age'] as int?,
+    );
+  }
+
+  static const serializer = SerializerFunc(fromJson: fromJson);
+
+  @override
+  String toString() => 'User(name: $name, age: $age)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is User && other.name == name && other.age == age;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ age.hashCode;
+}
---
+++ graphql_server/test/todos_schema.dart
@@ -0,0 +1,84 @@
+import 'package:graphql_schema/graphql_schema.dart';
+
+import 'common.dart';
+
+GraphQLSchema todosSchema() {
+  final userType = objectType<User>('User', fields: [
+    field(
+      'name',
+      graphQLString.nonNullable(),
+    ),
+    field(
+      'age',
+      graphQLInt,
+    ),
+  ]);
+
+  final todoType = objectType<Todo>('Todo', fields: [
+    field(
+      'text',
+      graphQLString,
+      resolve: (obj, args) => obj.text,
+    ),
+    field(
+      'completed',
+      graphQLBoolean,
+      resolve: (obj, args) => obj.completed,
+    ),
+    graphQLDate.field(
+      'time',
+      nullable: false,
+    ),
+    field(
+      'users',
+      listOf(userType),
+    ),
+  ]);
+
+  final serdeCtx = SerdeCtx();
+
+  serdeCtx.addAll([
+    Todo.serializer,
+    User.serializer,
+  ]);
+
+  final schema = graphQLSchema(
+    serdeCtx: serdeCtx,
+    queryType: objectType('api', fields: [
+      field(
+        'todos',
+        listOf(todoType),
+        resolve: (_, __) => [
+          Todo(
+            text: 'Clean your room!',
+            completed: false,
+            time: DateTime.now(),
+          )
+        ],
+      ),
+    ]),
+    mutationType: objectType(
+      'Mutation',
+      fields: [
+        field(
+          'completeTodo',
+          todoType,
+          inputs: [
+            GraphQLFieldInput(
+              'todoIn',
+              todoType.coerceToInputObject(),
+            ),
+          ],
+          resolve: (obj, vars) {
+            final todo = vars['todoIn'] as Todo;
+            return todo.copyWith(
+              completed: true,
+            );
+          },
+        ),
+      ],
+    ),
+  );
+
+  return schema;
+}
