abfc2a6b2fbc95ed1a30e35f49a199af5f24dfe2
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-09-06 00:33:33 +0000

    file graphql types

--- leto_shelf/example/schema/api_schema.dart
+++ leto_shelf/example/schema/api_schema.dart
@@ -1,22 +1,36 @@
+import 'dart:io';
+
 import 'package:graphql_schema/graphql_schema.dart';
 import 'package:shelf_graphql/shelf_graphql.dart';
 
 import 'books.controller.dart';
+import 'files.controller.dart';
+import 'safe_json.dart';
+import 'safe_json_graphql.dart';
 
-GraphQLSchema makeApiSchema() {
-  final controller = BooksController();
+GraphQLSchema makeApiSchema(FilesController filesController) {
+  final books = BooksController();
 
   final _bookType = bookType();
 
-  final mappedBooksStream = controller.stream.map(booksToJson);
-  final bookAddedStream = controller.bookAddedStream.map(
+  final mappedBooksStream = books.stream.map(booksToJson);
+  final bookAddedStream = books.bookAddedStream.map(
     (event) => event.toJson(),
   );
   bookAddedStream.listen((d) {
     print('bookAddedStream $d');
   });
 
+  final simpleError = objectType<Map<String, String>>(
+    'SimpleError',
+    fields: [
+      graphQLString.field('code', nullable: false),
+      graphQLString.field('message', nullable: false),
+    ],
+  );
+
   return graphQLSchema(
+    serdeCtx: SerdeCtx()..addAll([UploadedFileMeta.serializer]),
     queryType: objectType(
       'Queries',
       fields: [
@@ -24,8 +38,33 @@ GraphQLSchema makeApiSchema() {
           'books',
           listOf(_bookType),
           resolve: (obj, args) {
-            return booksToJson(controller.allBooks);
+            return booksToJson(books.allBooks);
           },
+        ),
+        field(
+          'files',
+          listOf(fileUploadType()),
+          resolve: (obj, args) {
+            return filesController.allFiles.values;
+          },
+        ),
+        field(
+          'fileEvents',
+          listOf(fileEventType()),
+          resolve: (obj, ctx) {
+            final filename = ctx.args['filename'] as String?;
+            return filename == null
+                ? filesController.history
+                : filesController.history.where(
+                    (element) => element.editedFilenames().contains(filename),
+                  );
+          },
+          inputs: [
+            GraphQLFieldInput(
+              'filename',
+              graphQLString,
+            ),
+          ],
         ),
       ],
     ),
@@ -35,13 +74,13 @@ GraphQLSchema makeApiSchema() {
         field(
           'createBook',
           _bookType,
-          resolve: (obj, args) {
+          resolve: (obj, ctx) {
             final book = Book(
-              name: args['name'] as String,
+              name: ctx.args['name'] as String,
               publicationDate: DateTime.now(),
               isFavourite: false,
             );
-            controller.add(book);
+            books.add(book);
             return book.toJson();
           },
           inputs: [
@@ -52,20 +91,54 @@ GraphQLSchema makeApiSchema() {
           ],
         ),
         field(
-          'addFileCommand',
-          fileUploadType(),
-          resolve: (obj, args) {
-            final upload = args['file'] as UploadedFile;
-            return {
-              'name': upload.filename,
-              'sizeInBytes': upload.sizeInBytes,
-              'mimeType': upload.contentType?.mimeType,
-            };
+          'addFile',
+          GraphQLUnionType(
+            'AddFileResult',
+            [fileUploadType(), simpleError],
+          ),
+          resolve: (obj, ctx) async {
+            final upload = ctx.args['file'] as UploadedFile;
+            final replace = ctx.args['replace'] as bool;
+            final extra = ctx.args['extra'] as Json?;
+            // TODO:
+            final meta = await upload.meta(
+                extra: extra?.toJson() as Map<String, Object?>?);
+            final result = filesController.consume(
+              FileEvent.added(meta, replace: replace),
+            );
+            return result.match(
+              (_) async {
+                final bytes = await upload.readAsBytes();
+                final file = File(
+                  relativeToScriptPath(['files', meta.filename]),
+                );
+                await file.writeAsBytes(bytes);
+                return meta;
+              },
+              (err) => {
+                'code': 'CANT_REPLACE',
+                'message': err,
+              },
+            );
+            // return {
+            //   'name': upload.filename,
+            //   'sizeInBytes': upload.sizeInBytes,
+            //   'mimeType': upload.contentType?.mimeType,
+            // };
           },
           inputs: [
             GraphQLFieldInput(
               'file',
               graphQLUpload.nonNullable(),
+            ),
+            GraphQLFieldInput(
+              'replace',
+              graphQLBoolean,
+              defaultValue: true,
+            ),
+            GraphQLFieldInput(
+              'extra',
+              graphQLJson,
             ),
           ],
         ),
@@ -86,6 +159,20 @@ GraphQLSchema makeApiSchema() {
           bookAddedType(_bookType),
           resolve: (obj, args) {
             return bookAddedStream;
+          },
+        ),
+        field(
+          'files',
+          listOf(fileUploadType()),
+          resolve: (obj, args) {
+            return filesController.fileChanges.map((event) => event.values);
+          },
+        ),
+        field(
+          'fileEvents',
+          fileEventType(),
+          resolve: (obj, args) {
+            return filesController.events;
           },
         ),
       ],
@@ -113,12 +200,13 @@ GraphQLObjectType<Book> bookType() {
   );
 }
 
-GraphQLObjectType<Map<String, Object?>> fileUploadType() {
-  return objectType(
+GraphQLObjectType<UploadedFileMeta>? _fileUploadType;
+GraphQLObjectType<UploadedFileMeta> fileUploadType() {
+  return _fileUploadType ??= objectType(
     'FileUpload',
     fields: [
       graphQLString.field(
-        'name',
+        'filename',
         nullable: false,
       ),
       graphQLInt.field(
@@ -127,7 +215,23 @@ GraphQLObjectType<Map<String, Object?>> fileUploadType() {
       ),
       graphQLString.field(
         'mimeType',
+        nullable: false,
       ),
+      graphQLDate.field(
+        'createdAt',
+        nullable: false,
+      ),
+      graphQLString.field(
+        'sha1Hash',
+        nullable: false,
+      ),
+      graphQLJson.field('extra'),
+      // TODO:
+      // graphQLString.field(
+      //   'url',
+      //   nullable: false,
+      //   resolve: (file, ctx) => 'http://localhost:8060/files/${file.filename}',
+      // ),
     ],
   );
 }
@@ -146,4 +250,13 @@ GraphQLObjectType<BookAdded> bookAddedType(GraphQLObjectType book) {
       ),
     ],
   );
+}
+
+String relativeToScriptPath(List<String> pathSegments) {
+  return [
+    '/',
+    ...Platform.script.pathSegments
+        .take(Platform.script.pathSegments.length - 1),
+    ...pathSegments
+  ].join(Platform.pathSeparator);
 }
