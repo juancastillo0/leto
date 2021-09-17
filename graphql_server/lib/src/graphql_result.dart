d2a133c81fd4dc0a707d46f915d4ce55b2444482
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-09-17 05:07:43 +0000

    exxtensions in graphql result

--- graphql_server/lib/src/graphql_result.dart
+++ graphql_server/lib/src/graphql_result.dart
@@ -11,34 +11,40 @@ class GraphQLResult {
   /// When [didExecute] is true, [data] is returned in the response
   final bool didExecute;
   final List<GraphQLExceptionError> errors;
+  final Map<String, Object?>? extensions;
 
   const GraphQLResult(
     this.data, {
     this.errors = const [],
     this.didExecute = true,
+    this.extensions,
   }) : assert(data == null || didExecute);
 
   GraphQLResult copyWith({
     Object? data,
     List<GraphQLExceptionError>? errors,
     bool? didExecute,
+    Map<String, Object?>? extensions,
   }) {
     return GraphQLResult(
       data ?? this.data,
       errors: errors ?? this.errors,
       didExecute: didExecute ?? this.didExecute,
+      extensions: extensions ?? this.extensions,
     );
   }
 
   Map<String, Object?> toJson() {
     return {
-      if (didExecute) 'data': data,
       if (errors.isNotEmpty) 'errors': errors.map((x) => x.toJson()).toList(),
+      if (didExecute) 'data': data,
+      if (extensions != null) 'extensions': extensions,
     };
   }
 
   @override
-  String toString() => 'GraphQLResult(data: $data, errors: $errors)';
+  String toString() => 'GraphQLResult(data: $data, errors: $errors, '
+      'didExecute: $didExecute, extensions: $extensions)';
 
   @override
   bool operator ==(Object other) {
@@ -48,9 +54,14 @@ class GraphQLResult {
     return other is GraphQLResult &&
         other.data == data &&
         other.didExecute == didExecute &&
+        listEquals(other.extensions, extensions) &&
         listEquals(other.errors, errors);
   }
 
   @override
-  int get hashCode => data.hashCode ^ errors.hashCode ^ didExecute.hashCode;
+  int get hashCode =>
+      data.hashCode ^
+      errors.hashCode ^
+      didExecute.hashCode ^
+      extensions.hashCode;
 }
