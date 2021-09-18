f8a2e04efd95418dfcb25cda0485b21b0d5372a6
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-09-18 22:33:42 +0000

    graphql-transport-ws subprotocol implementation, improve websocket server

--- graphql_server/lib/src/apollo/server.dart
+++ graphql_server/lib/src/apollo/server.dart
@@ -4,14 +4,37 @@ import 'package:graphql_server/src/graphql_result.dart';
 import 'remote_client.dart';
 import 'transport.dart';
 
+/// Error codes sent when closing the websocket channel in graphql-transport-ws
+///
+/// https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent/code
+/// https://github.com/enisdenjo/graphql-ws/blob/master/PROTOCOL.md
+class ErrorReason {
+  static const invalidMessage = 4400;
+  static const unauthorized = 4401;
+  static const duplicateSubscriptionId = 4409;
+  static const tooManyInitialisations = 4429;
+  static const initialisationTimeout = 4408;
+  static const normalClosure = 1000;
+  static const internalError = 1011;
+}
+
 abstract class Server {
+  static const supportedProtocols = ['graphql-transport-ws', 'graphql-ws'];
+
+  bool get isTransportWsProtocol => client.protocol == 'graphql-transport-ws';
+
   final RemoteClient client;
   final Duration? keepAliveInterval;
+  final Duration? connectionInitWaitTimeout;
+  late final Timer? _connectionInitTimer;
   final Completer _done = Completer<void>();
   late final StreamSubscription<OperationMessage> _sub;
 
   final Set<StreamSubscription<Object?>> _allSubs = {};
+  final Map<String, StreamSubscription<Object?>?> currentOperationIds = {};
+
   bool _init = false;
+  bool _receivedInit = false;
   Timer? _timer;
 
   Future<void> get done => _done.future;
@@ -20,20 +43,57 @@ abstract class Server {
     if (!_done.isCompleted) {
       _done.complete();
     }
+    _connectionInitTimer?.cancel();
     _timer?.cancel();
     await Future.wait(_allSubs.map((e) => e.cancel()));
   }
 
-  Server(this.client, {this.keepAliveInterval}) {
-    done.onError((Object e, StackTrace s) => _onDone());
+  Future<void> _connectionInitTimeout() {
+    return client.closeWithReason(
+      ErrorReason.initialisationTimeout,
+      'Connection initialisation timeout',
+    );
+  }
+
+  Server(
+    this.client, {
+    this.keepAliveInterval,
+    this.connectionInitWaitTimeout,
+  }) {
+    done.onError((Object e, StackTrace s) async {
+      if (isTransportWsProtocol) {
+        await client.closeWithReason(
+          e is FormatException
+              ? ErrorReason.invalidMessage
+              : ErrorReason.internalError,
+          e.toString(),
+        );
+      }
+      return _onDone();
+    });
+    _connectionInitTimer = connectionInitWaitTimeout == null
+        ? null
+        : Timer(connectionInitWaitTimeout!, _connectionInitTimeout);
 
     _sub = client.stream.listen(
       (msg) async {
-        if (msg.type == OperationMessage.gqlConnectionInit && !_init) {
+        if (msg.type == OperationMessage.gqlConnectionInit) {
+          _connectionInitTimer?.cancel();
+          if (_receivedInit) {
+            if (isTransportWsProtocol) {
+              return client.closeWithReason(
+                ErrorReason.tooManyInitialisations,
+                'Too many initialisation requests',
+              );
+            }
+            return;
+          }
+          _receivedInit = true;
+
           try {
-            Map? connectionParams;
+            Map<String, Object?>? connectionParams;
             if (msg.payload is Map) {
-              connectionParams = msg.payload as Map?;
+              connectionParams = (msg.payload as Map?)?.cast();
             } else if (msg.payload != null) {
               throw FormatException(
                   '${msg.type} payload must be a map (object).');
@@ -42,31 +102,60 @@ abstract class Server {
             final connect = await onConnect(client, connectionParams);
             if (!connect) throw false;
             _init = true;
-            client.sink
-                .add(OperationMessage(OperationMessage.gqlConnectionAck));
+            client.sink.add(
+              const OperationMessage(OperationMessage.gqlConnectionAck),
+            );
 
             if (keepAliveInterval != null) {
-              client.sink.add(
-                OperationMessage(OperationMessage.gqlConnectionKeepAlive),
+              final _keepAliveMsg = OperationMessage(
+                isTransportWsProtocol
+                    ? OperationMessage.pong
+                    : OperationMessage.gqlConnectionKeepAlive,
               );
+              client.sink.add(_keepAliveMsg);
               _timer ??= Timer.periodic(keepAliveInterval!, (timer) {
-                client.sink.add(
-                  OperationMessage(OperationMessage.gqlConnectionKeepAlive),
-                );
+                client.sink.add(_keepAliveMsg);
               });
             }
           } catch (e) {
-            if (e == false) {
-              _reportError('The connection was rejected.');
+            final _unauthorized = e == false;
+            final String message =
+                _unauthorized ? 'The connection was rejected.' : e.toString();
+
+            if (isTransportWsProtocol) {
+              return client.closeWithReason(
+                _unauthorized
+                    ? ErrorReason.unauthorized
+                    : e is FormatException
+                        ? ErrorReason.invalidMessage
+                        : ErrorReason.internalError,
+                message,
+              );
             } else {
-              _reportError(e.toString());
+              client.sink.add(
+                OperationMessage(
+                  OperationMessage.gqlConnectionError,
+                  payload: {'message': message},
+                ),
+              );
             }
           }
+        } else if (msg.type == OperationMessage.ping) {
+          client.sink.add(const OperationMessage(OperationMessage.pong));
         } else if (_init) {
-          if (msg.type == OperationMessage.gqlStart) {
+          if (msg.type == OperationMessage.subscribe ||
+              msg.type == OperationMessage.gqlStart) {
             if (msg.id == null) {
               throw FormatException('${msg.type} id is required.');
             }
+            if (isTransportWsProtocol &&
+                currentOperationIds.containsKey(msg.id)) {
+              return client.closeWithReason(
+                ErrorReason.duplicateSubscriptionId,
+                'Subscriber for ${msg.id} already exists',
+              );
+            }
+            currentOperationIds[msg.id!] = null;
             if (msg.payload == null) {
               throw FormatException('${msg.type} payload is required.');
             } else if (msg.payload is! Map) {
@@ -77,6 +166,7 @@ abstract class Server {
             final Object? query = payload['query'];
             final Object? variables = payload['variables'];
             final Object? operationName = payload['operationName'];
+            final Object? extensions = payload['extensions'];
             if (query is! String) {
               throw FormatException(
                   '${msg.type} payload must contain a string named "query".');
@@ -89,49 +179,53 @@ abstract class Server {
               throw FormatException(
                   '${msg.type} payload\'s "operationName" field must be a string.');
             }
+            if (extensions is! Map?) {
+              throw FormatException(
+                  '${msg.type} payload\'s "extensions" field must be a map (object).');
+            }
             final result = await onOperation(
               msg.id,
               query,
               variables?.cast<String, dynamic>(),
               operationName,
+              extensions?.cast<String, dynamic>(),
             );
-            Object? data = result.data;
-
-            if (result.errors.isNotEmpty) {
-              client.sink.add(OperationMessage(OperationMessage.gqlData,
-                  id: msg.id, payload: {'errors': result.errors.toList()}));
+            if (!result.didExecute) {
+              client.sink.add(OperationMessage(
+                OperationMessage.gqlError,
+                id: msg.id,
+                payload: result.errors,
+              ));
+              if (isTransportWsProtocol) {
+                currentOperationIds.remove(msg.id);
+                return;
+              }
             } else {
-              if (data is Map &&
-                  data.keys.length == 1 &&
-                  data.containsKey('data')) {
-                data = data['data'];
-              }
-
-              if (data is Stream) {
-                final sub = data.listen((Object? event) {
-                  if (event is Map &&
-                      event.keys.length == 1 &&
-                      event.containsKey('data')) {
-                    event = event['data'];
-                  }
-                  client.sink.add(OperationMessage(OperationMessage.gqlData,
-                      id: msg.id, payload: <String, Object?>{'data': event}));
-                });
-                _allSubs.add(sub);
-                await sub.asFuture<Object?>();
-                _allSubs.remove(sub);
-              } else {
-                client.sink.add(OperationMessage(OperationMessage.gqlData,
-                    id: msg.id, payload: {'data': data}));
-              }
+              await _sendData(msg.id!, result);
             }
-
-            // c.complete();
-            client.sink.add(
-                OperationMessage(OperationMessage.gqlComplete, id: msg.id));
+            // Don't send complete if the client completed the subscription
+            if (!isTransportWsProtocol ||
+                currentOperationIds.containsKey(msg.id)) {
+              client.sink.add(
+                  OperationMessage(OperationMessage.gqlComplete, id: msg.id));
+            }
+            currentOperationIds.remove(msg.id);
+          } else if (msg.type == OperationMessage.gqlComplete) {
+            if (msg.id == null) {
+              throw FormatException('${msg.type} id is required.');
+            }
+            final subs = currentOperationIds.remove(msg.id);
+            if (subs != null) {
+              await subs.cancel();
+            }
           } else if (msg.type == OperationMessage.gqlConnectionTerminate) {
             await _sub.cancel();
           }
+        } else if (msg.type == OperationMessage.subscribe) {
+          return client.closeWithReason(
+            ErrorReason.unauthorized,
+            'Unauthorized',
+          );
         }
       },
       onError: _done.completeError,
@@ -140,21 +234,45 @@ abstract class Server {
     );
   }
 
-  void _reportError(String message) {
-    client.sink.add(
-      OperationMessage(
-        OperationMessage.gqlConnectionError,
-        payload: {'message': message},
-      ),
-    );
+  Future<void> _sendData(String id, GraphQLResult result) async {
+    final msgType = isTransportWsProtocol
+        ? OperationMessage.next
+        : OperationMessage.gqlData;
+
+    final subscriptionStream = result.subscriptionStream;
+    if (subscriptionStream != null) {
+      final sub = subscriptionStream.listen((GraphQLResult event) {
+        if (_done.isCompleted) {
+          return;
+        }
+        client.sink.add(OperationMessage(
+          msgType,
+          id: id,
+          payload: event.toJson(),
+        ));
+      });
+      _allSubs.add(sub);
+      await sub.asFuture<Object?>();
+      _allSubs.remove(sub);
+    } else {
+      client.sink.add(OperationMessage(
+        msgType,
+        id: id,
+        payload: result.toJson(),
+      ));
+    }
   }
 
-  FutureOr<bool> onConnect(RemoteClient client, [Map? connectionParams]);
+  FutureOr<bool> onConnect(
+    RemoteClient client, [
+    Map<String, Object?>? connectionParams,
+  ]);
 
   FutureOr<GraphQLResult> onOperation(
     String? id,
     String query, [
     Map<String, dynamic>? variables,
     String? operationName,
+    Map<String, Object?>? extensions,
   ]);
 }
