3d020ebcbecc27269f6092aaabdb9253cd1d651f
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-09-26 20:21:42 +0000

    execute resolve test port

--- leto_shelf/test/star_wars/executor/resolve_test.dart
+++ leto_shelf/test/star_wars/executor/resolve_test.dart
@@ -1,130 +1,152 @@
 // https://github.com/graphql/graphql-js/blob/8261922bafb8c2b5c5041093ce271bdfcdf133c3/src/execution/__tests__/resolve-test.ts
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
 
-import { parse } from '../../language/parser';
+import 'dart:convert';
 
-import type { GraphQLFieldConfig } from '../../type/definition';
-import { GraphQLSchema } from '../../type/schema';
-import { GraphQLInt, GraphQLString } from '../../type/scalars';
-import { GraphQLObjectType } from '../../type/definition';
+import 'package:shelf_graphql/shelf_graphql.dart';
+import 'package:test/test.dart';
 
-import { executeSync } from '../execute';
+class Adder {
+  final int _num;
 
-describe('Execute: resolve function', () => {
-  function testSchema(testField: GraphQLFieldConfig<any, any>) {
-    return new GraphQLSchema({
-      query: new GraphQLObjectType({
-        name: 'Query',
-        fields: {
-          test: testField,
+  const Adder(this._num);
+
+  int test(Map<String, Object?> args, Map<Object, Object?> context) {
+    return _num + (args['addend1'] as int) + (context['addend2'] as int);
+  }
+}
+
+/// Execute: resolve function
+void main() {
+  GraphQLSchema testSchema(
+    GraphQLObjectField<Object, Object, Object> testField,
+  ) {
+    return GraphQLSchema(
+      queryType: objectType(
+        'Query',
+        fieldsMap: {
+          'test': testField,
         },
-      }),
-    });
+      ),
+    );
   }
 
-  it('default function accesses properties', () => {
-    const result = executeSync({
-      schema: testSchema({ type: GraphQLString }),
-      document: parse('{ test }'),
-      rootValue: { test: 'testValue' },
-    });
+  Future<Map<String, Object?>> execute(
+    GraphQLSchema schema,
+    String query, {
+    Map<String, Object?>? variableValues,
+    Map<String, Object?>? globalVariables,
+    Object? rootValue,
+  }) async {
+    final values = await GraphQL(schema).parseAndExecute(
+      query,
+      variableValues: variableValues,
+      initialValue: rootValue,
+      globalVariables: globalVariables,
+    );
+    return values.toJson();
+  }
 
-    expect(result).to.deep.equal({
-      data: {
-        test: 'testValue',
+  test('default function accesses properties', () async {
+    final result = await execute(
+      testSchema(graphQLString.fieldSpec()),
+      '{ test }',
+      rootValue: {'test': 'testValue'},
+    );
+
+    expect(result, {
+      'data': {
+        'test': 'testValue',
       },
     });
   });
 
-  it('default function calls methods', () => {
-    const rootValue = {
-      _secret: 'secretValue',
-      test() {
-        return this._secret;
+  test('default function calls methods', () async {
+    final rootValue = {
+      '_secret': 'secretValue',
+      'test': () {
+        return 'secretValue'; //_secret;
       },
     };
 
-    const result = executeSync({
-      schema: testSchema({ type: GraphQLString }),
-      document: parse('{ test }'),
-      rootValue,
-    });
-    expect(result).to.deep.equal({
-      data: {
-        test: 'secretValue',
+    final result = await execute(
+      testSchema(graphQLString.fieldSpec()),
+      '{ test }',
+      rootValue: rootValue,
+    );
+    expect(result, {
+      'data': {
+        'test': 'secretValue',
       },
     });
   });
 
-  it('default function passes args and context', () => {
-    class Adder {
-      _num: number;
+  test('default function passes args and context', () async {
+    const rootValue = Adder(700);
 
-      constructor(num: number) {
-        this._num = num;
-      }
+    final schema = testSchema(
+      graphQLInt.fieldSpec<Adder>(
+        inputs: [GraphQLFieldInput('addend1', graphQLInt)],
+        resolve: (obj, ctx) => obj.test(ctx.args, ctx.globals),
+      ),
+    );
+    final contextValue = {'addend2': 9};
+    const document = '{ test(addend1: 80) }';
 
-      test(args: { addend1: number }, context: { addend2: number }) {
-        return this._num + args.addend1 + context.addend2;
-      }
-    }
-    const rootValue = new Adder(700);
-
-    const schema = testSchema({
-      type: GraphQLInt,
-      args: {
-        addend1: { type: GraphQLInt },
-      },
-    });
-    const contextValue = { addend2: 9 };
-    const document = parse('{ test(addend1: 80) }');
-
-    const result = executeSync({ schema, document, rootValue, contextValue });
-    expect(result).to.deep.equal({
-      data: { test: 789 },
+    final result = await execute(
+      schema,
+      document,
+      rootValue: rootValue,
+      globalVariables: contextValue,
+    );
+    expect(result, {
+      'data': {'test': 789},
     });
   });
 
-  it('uses provided resolve function', () => {
-    const schema = testSchema({
-      type: GraphQLString,
-      args: {
-        aStr: { type: GraphQLString },
-        aInt: { type: GraphQLInt },
-      },
-      resolve: (source, args) => JSON.stringify([source, args]),
-    });
+  test('uses provided resolve function', () async {
+    final schema = testSchema(graphQLString.fieldSpec(
+      inputs: [
+        GraphQLFieldInput('aStr', graphQLString),
+        GraphQLFieldInput('aInt', graphQLInt),
+      ],
+      resolve: (parent, ctx) => jsonEncode([parent, ctx.args]),
+    ));
 
-    function executeQuery(query: string, rootValue?: unknown) {
-      const document = parse(query);
-      return executeSync({ schema, document, rootValue });
-    }
-
-    expect(executeQuery('{ test }')).to.deep.equal({
-      data: {
-        test: '[null,{}]',
+    expect(await execute(schema, '{ test }'), {
+      'data': {
+        // TODO: in graphql-js it is 'test': '[null,{}]',
+        // we restrict the parent as a non null value and
+        // pass the global variables as the root object
+        'test': '[{},{}]',
       },
     });
 
-    expect(executeQuery('{ test }', 'Source!')).to.deep.equal({
-      data: {
-        test: '["Source!",{}]',
-      },
-    });
-
-    expect(executeQuery('{ test(aStr: "String!") }', 'Source!')).to.deep.equal({
-      data: {
-        test: '["Source!",{"aStr":"String!"}]',
+    expect(await execute(schema, '{ test }', rootValue: 'Source!'), {
+      'data': {
+        'test': '["Source!",{}]',
       },
     });
 
     expect(
-      executeQuery('{ test(aInt: -123, aStr: "String!") }', 'Source!'),
-    ).to.deep.equal({
-      data: {
-        test: '["Source!",{"aStr":"String!","aInt":-123}]',
+        await execute(schema, '{ test(aStr: "String!") }',
+            rootValue: 'Source!'),
+        {
+          'data': {
+            'test': '["Source!",{"aStr":"String!"}]',
+          },
+        });
+
+    expect(
+      await execute(
+        schema,
+        '{ test(aInt: -123, aStr: "String!") }',
+        rootValue: 'Source!',
+      ),
+      {
+        'data': {
+          'test': '["Source!",{"aStr":"String!","aInt":-123}]',
+        },
       },
-    });
+    );
   });
-});
+}
