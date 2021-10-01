92442f9ba95e6c9f9bc4095fce79ce37a9024491
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-10-01 04:18:57 +0000

    improve convertType error and input type mismatch

--- graphql_server/lib/src/schema_helpers.dart
+++ graphql_server/lib/src/schema_helpers.dart
@@ -26,12 +26,13 @@ class GraphQLValueComputer extends SimpleVisitor<Object> {
 
     if (_targetType == null) {
       throw GraphQLException.fromSourceSpan(
-          'An enum value was given, but in this context,'
-          ' its type cannot be deduced.',
-          span);
+        'An enum value was given, but in this context,'
+        ' its type cannot be deduced.',
+        span,
+      );
     } else if (_targetType is! GraphQLEnumType) {
       throw GraphQLException.fromSourceSpan(
-          'An enum value was given, but the type'
+          'An enum value (${node.name.value}) was given, but the expected type'
           ' "$targetType" is not an enum.',
           span);
     } else {
@@ -138,8 +139,10 @@ GraphQLType convertType(
         default:
           return customTypes.firstWhere(
             (t) => t.name == node.name.value,
-            orElse: () => throw ArgumentError(
-              'Unknown GraphQL type: "${node.name.value}"',
+            orElse: () => throw GraphQLExceptionError(
+              'Unknown GraphQL type: "${node.name.value}".',
+              locations: GraphQLErrorLocation.listFromSource(
+                  node.span?.start ?? node.name.span?.start),
             ),
           );
       }
