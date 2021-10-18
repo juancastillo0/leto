a4043f0e57b6fce60dbe2b213d6e6c22a5518cb7
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-10-18 21:49:42 +0000

    use provider container and UncontrolledProviderScope

--- leto_shelf/chat_example/lib/main.dart
+++ leto_shelf/chat_example/lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:chat_example/api/auth_store.dart';
 import 'package:chat_example/api/client.dart';
 import 'package:chat_example/chats.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
-  final client = await initClient();
-
-  runApp(ProviderScope(
-    overrides: [
-      clientProvider.overrideWithValue(client),
-    ],
+  final providerContainer = await initClient();
+  runApp(UncontrolledProviderScope(
+    container: providerContainer,
+    // overrides: [
+    //   clientProvider.overrideWithValue(client),
+    // ],
     child: const MyApp(),
   ));
 }
@@ -20,58 +21,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GraphQL Chat Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ChatsPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Consumer(builder: (context, ref, _) {
+        final state = ref.watch(authStoreProv);
+        if (!state.isLoggedIn) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return const ChatsPage();
+      }),
     );
   }
 }
