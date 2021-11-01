1d7d084e341e5b3eec652ee8e3f0132cbf9199e7
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-01 19:05:06 +0000

    dont use freezed for json type

--- leto_shelf/example/lib/types/safe_json.dart
+++ leto_shelf/example/lib/types/safe_json.dart
@@ -1,12 +1,10 @@
 import 'dart:convert';
-
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:collection/collection.dart';
 import 'package:oxidized/oxidized.dart';
 
-part 'safe_json.freezed.dart';
+export 'safe_json_graphql.dart';
 
-@freezed
-class Json with _$Json {
+abstract class Json {
   const Json._();
   const factory Json.map(Map<String, Json> value) = JsonMap;
   const factory Json.list(List<Json> value) = JsonList;
@@ -16,7 +14,34 @@ class Json with _$Json {
   const factory Json.str(String value) = JsonStr;
   const factory Json.none() = JsonNone;
 
+  Object? get value;
+
   static const null_ = JsonNone();
+
+  T when<T>({
+    required T Function(Map<String, Json>) map,
+    required T Function(List<Json>) list,
+    required T Function(num) number,
+    required T Function(bool) boolean,
+    required T Function(String) str,
+    required T Function(Null) none,
+  }) {
+    final v = this;
+    if (v is JsonMap) {
+      return map(v.value);
+    } else if (v is JsonList) {
+      return list(v.value);
+    } else if (v is JsonNumber) {
+      return number(v.value);
+    } else if (v is JsonBoolean) {
+      return boolean(v.value);
+    } else if (v is JsonStr) {
+      return str(v.value);
+    } else if (v is JsonNone) {
+      return none(null);
+    }
+    throw Error();
+  }
 
   Object? toJson({bool shallow = false}) {
     final v = this;
@@ -24,8 +49,7 @@ class Json with _$Json {
       return null;
     }
     if (shallow) {
-      // ignore: avoid_dynamic_calls
-      return (v as dynamic).value;
+      return value;
     } else {
       return when(
         map: (map) => map.map(
@@ -35,7 +59,7 @@ class Json with _$Json {
         number: (n) => n,
         str: (str) => str,
         boolean: (b) => b,
-        none: () => null,
+        none: (_) => null,
       );
     }
   }
@@ -142,4 +166,41 @@ class ResultCtx<T extends Object, E extends Object> {
   Never throwErr(E err) {
     throw err;
   }
+}
+
+class JsonMap extends Json {
+  @override
+  final Map<String, Json> value;
+  const JsonMap(this.value) : super._();
+}
+
+class JsonList extends Json {
+  @override
+  final List<Json> value;
+  const JsonList(this.value) : super._();
+}
+
+class JsonNumber extends Json {
+  @override
+  final num value;
+  const JsonNumber(this.value) : super._();
+}
+
+class JsonBoolean extends Json {
+  @override
+  final bool value;
+  // ignore: avoid_positional_boolean_parameters
+  const JsonBoolean(this.value) : super._();
+}
+
+class JsonStr extends Json {
+  @override
+  final String value;
+  const JsonStr(this.value) : super._();
+}
+
+class JsonNone extends Json {
+  @override
+  Object? get value => null;
+  const JsonNone() : super._();
 }
