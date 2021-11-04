a92327d821212a3ea55b4fe343c08569956116a9
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-04 18:37:35 +0000

    schema test in generator example and export all generator tests

---
+++ leto_generator/example/lib/test/schema_test.dart
@@ -0,0 +1,39 @@
+import 'package:leto_generator_example/decimal.dart';
+import 'package:leto_generator_example/graphql_api.schema.dart';
+import 'package:leto_schema/leto_schema.dart';
+import 'package:leto_schema/utilities.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('schema generator', () {
+    final built = buildSchema(graphqlApiSchema.schemaStr);
+
+    expect(printSchema(built), graphqlApiSchema.schemaStr);
+  });
+
+  test('schema decimal', () {
+    final schema = GraphQLSchema(
+      queryType: objectType(
+        'Query',
+        fields: [
+          decimalGraphQLType.field(
+            'decimal',
+            inputs: [
+              inputField(
+                'input',
+                decimalGraphQLType,
+                defaultValue: Decimal.zero,
+              ),
+            ],
+            resolve: (obj, ctx) {
+              return ctx.args['input']! as Decimal;
+            },
+          ),
+        ],
+      ),
+    );
+    final built = buildSchema(schema.schemaStr);
+
+    expect(printSchema(built), schema.schemaStr);
+  });
+}
---
+++ leto_schema/test/generator_test.dart
@@ -0,0 +1,12 @@
+import 'package:leto_generator_example/test/input_test.dart' as input_test;
+import 'package:leto_generator_example/test/interface_test.dart'
+    as interface_test;
+import 'package:leto_generator_example/test/schema_test.dart' as schema_test;
+import 'package:leto_generator_example/test/union_test.dart' as union_test;
+
+void main() {
+  input_test.main();
+  interface_test.main();
+  union_test.main();
+  schema_test.main();
+}
