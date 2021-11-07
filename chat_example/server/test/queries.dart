f150ba6fcac3e8865db15af004f3ea486feefca8
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-07 04:34:34 +0000

    reorder room test and subscription test in chat example

---
+++ chat_example/server/test/queries.dart
@@ -0,0 +1,85 @@
+const subscriptionQuery = '''
+subscription onEvent {
+  onEvent {
+    ...BaseEvent
+  }
+}
+
+fragment BaseEvent on DBEvent {
+  id
+  type
+  userId
+  sessionId
+  createdAt
+  data {
+    __typename
+    ... on ChatMessageDBEventData {
+      ...BaseChatMessageDBEventData
+    }
+  }
+}
+        
+fragment BaseChatMessageDBEventData on ChatMessageDBEventData {
+  value {
+    ... on ChatMessageSentEvent {
+      message {
+        ...FullMessage
+      }
+    }
+  }
+}
+
+fragment FullMessage on ChatMessage {
+  ...BaseMessage
+  referencedMessage {
+    ...BaseMessage
+  }
+}
+
+fragment BaseMessage on ChatMessage {
+  id
+  chatId
+  userId
+  message
+  type
+  fileUrl
+  createdAt
+}''';
+
+const messagesQuery = r'''
+query getMessages($chatId: Int!) {
+  getMessage(chatId: $chatId) {
+    ...FullMessage
+  }
+}
+
+mutation sendMessage(
+  $chatId: Int!
+  $message: String!
+  $referencedMessageId: Int
+) {
+  sendMessage(
+    chatId: $chatId
+    message: $message
+    referencedMessageId: $referencedMessageId
+  ) {
+    ...FullMessage
+  }
+}
+
+fragment FullMessage on ChatMessage {
+  ...BaseMessage
+  referencedMessage {
+    ...BaseMessage
+  }
+}
+
+fragment BaseMessage on ChatMessage {
+  id
+  chatId
+  userId
+  message
+  type
+  fileUrl
+  createdAt
+}''';
--- chat_example/server/test/server_test.dart
+++ chat_example/server/test/server_test.dart
@@ -11,9 +11,30 @@ import 'package:server/handler.dart';
 import 'package:sqlite3/sqlite3.dart';
 import 'package:test/test.dart';
 
+import 'queries.dart';
+import 'web_socket_link.dart';
+
+class _TestUser {
+  final String refreshToken;
+  final String accessToken;
+  final String name;
+  final int chatId;
+  final int userId;
+
+  _TestUser({
+    required this.refreshToken,
+    required this.accessToken,
+    required this.name,
+    required this.userId,
+    required this.chatId,
+  });
+}
+
 void main() {
   late HttpServer server;
   String host() => 'http://${server.address.host}:${server.port}';
+  String subscriptionUrl() =>
+      'ws://${server.address.host}:${server.port}/graphql-subscription';
 
   Uri url() => Uri.parse('${host()}/graphql');
 
@@ -68,27 +89,38 @@ void main() {
       );
     }
 
-    test('Create user and room', () async {
+    Future<_TestUser> _createdUserAndRoom({required String chatName}) async {
       var response = await send(
         const GraphQLRequest(
           query: 'mutation signUp { signUp(name: "aa", password: "aaaaaa")'
-              ' { ... on TokenWithUser {accessToken} } }',
+              ' { ... on TokenWithUser {accessToken refreshToken'
+              ' user { id name } } } }',
         ),
       );
       expect(response.statusCode, 200);
       dynamic body = jsonDecode(response.body);
       expect(body, {
         'data': {
-          'signUp': {'accessToken': const TypeMatcher<String>()}
+          'signUp': {
+            'accessToken': const TypeMatcher<String>(),
+            'refreshToken': const TypeMatcher<String>(),
+            'user': {
+              'id': isA<int>(),
+              'name': 'aa',
+            }
+          }
         }
       });
 
       // ignore: avoid_dynamic_calls
-      final accessToken = body['data']['signUp']['accessToken'] as String;
+      final data = body['data']['signUp'] as Map<String, dynamic>;
+      final accessToken = data['accessToken'] as String;
+      final refreshToken = data['refreshToken'] as String;
+      final userId = data['user']['id'] as int;
 
       response = await send(
-        const GraphQLRequest(
-          query: 'mutation createChatRoom { createChatRoom(name: "aachat")'
+        GraphQLRequest(
+          query: 'mutation createChatRoom { createChatRoom(name: "$chatName")'
               ' { name, createdAt } }',
         ),
       );
@@ -108,9 +140,9 @@ void main() {
       });
 
       response = await send(
-        const GraphQLRequest(
-          query: 'mutation createChatRoom { createChatRoom(name: "aachat")'
-              ' { name } }',
+        GraphQLRequest(
+          query: 'mutation createChatRoom { createChatRoom(name: "$chatName")'
+              ' { id name } }',
         ),
         headers: {
           HttpHeaders.authorizationHeader: accessToken,
@@ -120,11 +152,25 @@ void main() {
       body = jsonDecode(response.body);
       expect(body, {
         'data': {
-          'createChatRoom': {'name': 'aachat'}
+          'createChatRoom': {'name': chatName, 'id': isA<int>()}
         },
       });
 
-      response = await send(
+      return _TestUser(
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        name: 'aa',
+        userId: userId,
+        chatId: (((body as Map)['data'] as Map)['createChatRoom'] as Map)['id']
+            as int,
+      );
+    }
+
+    test('Create user and room', () async {
+      const chatName = 'a_chat';
+      final user = await _createdUserAndRoom(chatName: chatName);
+      final accessToken = user.accessToken;
+      var response = await send(
         const GraphQLRequest(
           query: 'query getChatRooms { getChatRooms '
               ' { name, users { role user { name } } } }',
@@ -139,11 +185,11 @@ void main() {
         },
       );
       expect(response.statusCode, 200);
-      body = jsonDecode(response.body);
+      Object? body = jsonDecode(response.body);
       const _payload = {
         'getChatRooms': [
           {
-            'name': 'aachat',
+            'name': chatName,
             'users': [
               {
                 'role': 'admin',
@@ -187,6 +233,73 @@ void main() {
           }
         }
       });
+    });
+
+    test('send chat messages', () async {
+      const chatName = 'a_chat';
+      final user = await _createdUserAndRoom(chatName: chatName);
+
+      final link = WebSocketLink(subscriptionUrl(), initialPayload: {
+        'refreshToken': user.refreshToken,
+      });
+
+      int eventIndex = 0;
+      const messagesToSend = ['msg test', 'msg test response'];
+
+      link.requestRaw(subscriptionQuery).listen(
+            expectAsync1(
+              (event) {
+                expect(event.errors, isNull);
+
+                expect(event.data!['onEvent']['userId'], user.userId);
+
+                final message = event.data!['onEvent']['data']['value']
+                    ['message'] as Map<String, Object?>;
+
+                expect(
+                  message['referencedMessage'],
+                  eventIndex == 0 ? isNull : isNotNull,
+                );
+                expect(message['message'], messagesToSend[eventIndex++]);
+              },
+              count: 2,
+            ),
+          );
+      await Future<void>.delayed(const Duration(seconds: 2));
+
+      final response = await link.requestRaw(
+        messagesQuery,
+        operationName: 'sendMessage',
+        variables: {'message': messagesToSend[0], 'chatId': user.chatId},
+      ).first;
+
+      expect(response.errors, isNull);
+
+      final messageId = response.data!['sendMessage']['id'] as int;
+
+      final response2 = await link.requestRaw(
+        messagesQuery,
+        operationName: 'sendMessage',
+        variables: {
+          'message': messagesToSend[1],
+          'chatId': user.chatId,
+          'referencedMessageId': messageId,
+        },
+      ).first;
+
+      final messageId2 = response2.data!['sendMessage']['id'] as int;
+
+      final responseMessages = await link.requestRaw(
+        messagesQuery,
+        operationName: 'getMessages',
+        variables: {
+          'chatId': user.chatId,
+        },
+      ).first;
+
+      final messages = responseMessages.data!['getMessage'] as List;
+      expect(messages[0]['id'], messageId);
+      expect(messages[1]['id'], messageId2);
     });
   });
 }
