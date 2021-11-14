2eeebca8de97f4cdb7b1da69140bfc8a493adde0
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-14 17:03:55 +0000

    VariableUsage, NodeWithSelectionSet, fragmentsMap and other utilities in ValidationCtx

--- leto_schema/lib/src/rules/validate.dart
+++ leto_schema/lib/src/rules/validate.dart
@@ -1,6 +1,15 @@
 import 'package:gql/ast.dart';
+
 import 'package:leto_schema/leto_schema.dart';
+import 'package:leto_schema/src/rules/rules/no_undefined_variables_rule.dart';
+import 'package:leto_schema/src/rules/rules/possible_fragment_spread_rule.dart';
+import 'package:leto_schema/src/rules/rules/scalar_leafs_rule.dart';
+import 'package:leto_schema/src/rules/rules/single_field_subscription_rule.dart';
+import 'package:leto_schema/src/rules/rules/unique_variable_names_rule.dart';
 import 'package:leto_schema/src/rules/type_info.dart';
+import 'package:leto_schema/src/rules/typed_visitor.dart';
+import 'package:leto_schema/utilities.dart';
+import 'package:meta/meta.dart';
 
 import 'executable_rules.dart';
 
@@ -32,6 +41,11 @@ List<GraphQLError> validateDocument(
     knownFragmentNamesRule(ctx),
     //
     fieldsOnCorrectTypeRule(ctx),
+    singleFieldSubscriptionsRule(ctx),
+    possibleFragmentSpreadsRule(ctx),
+    uniqueVariableNamesRule(ctx),
+    // TODO: NoFragmentCyclesRule,
+    noUndefinedVariablesRule(ctx),
   ], onAccept: (result) {
     if (result is List<GraphQLError>) {
       // TODO: these are returning error when visiting, not by using reportError
@@ -52,6 +66,19 @@ class ValidationCtx {
   final TypeInfo typeInfo;
   final int maxErrors;
 
+  late final Map<String, FragmentDefinitionNode> fragmentsMap =
+      fragmentsFromDocument(document);
+
+  final Map<NodeWithSelectionSet, List<VariableUsage>> _variableUsages = {};
+
+  final Map<OperationDefinitionNode, List<VariableUsage>>
+      _recursiveVariableUsages = {};
+
+  final Map<OperationDefinitionNode, List<FragmentDefinitionNode>>
+      _recursivelyReferencedFragments = {};
+
+  final Map<SelectionSetNode, List<FragmentSpreadNode>> _fragmentSpreads = {};
+
   ///
   ValidationCtx(
     this.schema,
@@ -68,6 +95,146 @@ class ValidationCtx {
       throw _AbortValidationException();
     }
   }
+
+  List<VariableUsage> getVariableUsages(NodeWithSelectionSet node) {
+    var usages = _variableUsages[node];
+    if (usages == null) {
+      final newUsages = <VariableUsage>[];
+      final typeInfo = TypeInfo(schema);
+
+      final _visitor = TypedVisitor();
+      // TOOD:
+      _visitor.add<VariableDefinitionNode>((_) => false);
+      _visitor.add<VariableNode>((variable) {
+        newUsages.add(VariableUsage(
+          node: variable,
+          type: typeInfo.getInputType(),
+          defaultValue: typeInfo.getDefaultValue(),
+        ));
+      });
+      node.node.accept(
+        WithTypeInfoVisitor(typeInfo, visitors: [_visitor]),
+      );
+      usages = newUsages;
+      _variableUsages[node] = usages;
+    }
+    return usages;
+  }
+
+  List<VariableUsage> getRecursiveVariableUsages(
+    OperationDefinitionNode operation,
+  ) {
+    var usages = _recursiveVariableUsages[operation];
+    if (usages == null) {
+      usages = getVariableUsages(NodeWithSelectionSet.operation(operation));
+      for (final frag in getRecursivelyReferencedFragments(operation)) {
+        usages = usages!
+            .followedBy(getVariableUsages(NodeWithSelectionSet.fragment(frag)))
+            .toList();
+      }
+      _recursiveVariableUsages[operation] = usages!;
+    }
+    return usages;
+  }
+
+  List<FragmentDefinitionNode> getRecursivelyReferencedFragments(
+    OperationDefinitionNode operation,
+  ) {
+    var fragments = this._recursivelyReferencedFragments[operation];
+    if (fragments == null) {
+      fragments = [];
+      final collectedNames = <String, bool>{};
+      final nodesToVisit = <SelectionSetNode>[operation.selectionSet];
+      while (nodesToVisit.length != 0) {
+        final node = nodesToVisit.removeLast();
+        for (final spread in this.getFragmentSpreads(node)) {
+          final fragName = spread.name.value;
+          if (collectedNames[fragName] != true) {
+            collectedNames[fragName] = true;
+            final fragment = this.fragmentsMap[fragName];
+            if (fragment != null) {
+              // @ts-expect-error FIXME: TS Conversion
+              fragments.add(fragment);
+              nodesToVisit.add(fragment.selectionSet);
+            }
+          }
+        }
+      }
+      this._recursivelyReferencedFragments[operation] = fragments;
+    }
+    return fragments;
+  }
+
+  List<FragmentSpreadNode> getFragmentSpreads(
+    SelectionSetNode node,
+  ) {
+    var spreads = _fragmentSpreads[node];
+    if (spreads == null) {
+      spreads = [];
+      final setsToVisit = <SelectionSetNode>[node];
+      while (setsToVisit.length != 0) {
+        final set = setsToVisit.removeLast();
+        for (final selection in set.selections) {
+          if (selection is FragmentSpreadNode) {
+            // @ts-expect-error FIXME: TS Conversion
+            spreads.add(selection);
+          } else if (selection.selectionSet != null) {
+            setsToVisit.add(selection.selectionSet!);
+          }
+        }
+      }
+      _fragmentSpreads[node] = spreads;
+    }
+    return spreads;
+  }
 }
 
 class _AbortValidationException implements Exception {}
+
+@immutable
+abstract class NodeWithSelectionSet {
+  ///
+  const NodeWithSelectionSet();
+
+  Node get node;
+
+  const factory NodeWithSelectionSet.operation(OperationDefinitionNode node) =
+      _NodeWithSelectionSetOperationDefinition;
+  const factory NodeWithSelectionSet.fragment(FragmentDefinitionNode node) =
+      _NodeWithSelectionSetFragmentDefinition;
+
+  @override
+  bool operator ==(Object other) =>
+      other is NodeWithSelectionSet && node == other.node;
+
+  @override
+  int get hashCode => node.hashCode;
+}
+
+class _NodeWithSelectionSetOperationDefinition extends NodeWithSelectionSet {
+  @override
+  final OperationDefinitionNode node;
+
+  const _NodeWithSelectionSetOperationDefinition(this.node);
+}
+
+class _NodeWithSelectionSetFragmentDefinition extends NodeWithSelectionSet {
+  @override
+  final FragmentDefinitionNode node;
+
+  const _NodeWithSelectionSetFragmentDefinition(this.node);
+}
+
+class VariableUsage {
+  final VariableNode node;
+  final GraphQLType? type;
+  final Object? defaultValue;
+
+  /// The position and information about a variable within
+  /// a GraphQL document
+  VariableUsage({
+    required this.node,
+    this.type,
+    this.defaultValue,
+  });
+}
