13f53b2ca73dd9680ba6d56ceecf672d5365259c
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-14 17:31:30 +0000

    all validations in validate

--- leto_schema/lib/src/rules/validate.dart
+++ leto_schema/lib/src/rules/validate.dart
@@ -1,11 +1,19 @@
 import 'package:gql/ast.dart';
 
 import 'package:leto_schema/leto_schema.dart';
+import 'package:leto_schema/src/rules/rules/known_argument_names_rule.dart';
+import 'package:leto_schema/src/rules/rules/known_directive_rule.dart';
 import 'package:leto_schema/src/rules/rules/no_undefined_variables_rule.dart';
+import 'package:leto_schema/src/rules/rules/no_unused_variables_rule.dart';
 import 'package:leto_schema/src/rules/rules/possible_fragment_spread_rule.dart';
+import 'package:leto_schema/src/rules/rules/provide_required_arguments_rule.dart';
 import 'package:leto_schema/src/rules/rules/scalar_leafs_rule.dart';
 import 'package:leto_schema/src/rules/rules/single_field_subscription_rule.dart';
+import 'package:leto_schema/src/rules/rules/unique_argument_names_rule.dart';
+import 'package:leto_schema/src/rules/rules/unique_directive_per_location_rule.dart';
+import 'package:leto_schema/src/rules/rules/unique_input_field_name_rule.dart';
 import 'package:leto_schema/src/rules/rules/unique_variable_names_rule.dart';
+import 'package:leto_schema/src/rules/rules/variables_in_allowed_position_rule.dart';
 import 'package:leto_schema/src/rules/type_info.dart';
 import 'package:leto_schema/src/rules/typed_visitor.dart';
 import 'package:leto_schema/utilities.dart';
@@ -31,21 +39,32 @@ List<GraphQLError> validateDocument(
   );
 
   final visitor = WithTypeInfoVisitor(typeInfo, visitors: [
-    // TODO: these are returning error when visiting, not by using reportError
+    // TODO: some are returning error when visiting, not by using reportError
+    executableDefinitionsRule(ctx),
     uniqueOperationNamesRule(ctx),
     loneAnonymousOperationRule(ctx),
+    singleFieldSubscriptionsRule(ctx),
     knownTypeNamesRule(ctx),
     fragmentsOnCompositeTypesRule(ctx),
     variablesAreInputTypesRule(ctx),
     scalarLeafsRule(ctx),
-    knownFragmentNamesRule(ctx),
-    //
     fieldsOnCorrectTypeRule(ctx),
-    singleFieldSubscriptionsRule(ctx),
+    knownFragmentNamesRule(ctx),
+    noUnusedFragmentsRule(ctx),
     possibleFragmentSpreadsRule(ctx),
-    uniqueVariableNamesRule(ctx),
     // TODO: NoFragmentCyclesRule,
+    uniqueVariableNamesRule(ctx),
     noUndefinedVariablesRule(ctx),
+    noUnusedVariablesRule(ctx),
+    knownDirectivesRule(ctx),
+    uniqueDirectivesPerLocationRule(ctx),
+    knownArgumentNamesRule(ctx),
+    uniqueArgumentNamesRule(ctx),
+    // TODO: ValuesOfCorrectType
+    providedRequiredArgumentsRule(ctx),
+    variablesInAllowedPositionRule(ctx),
+    // TODO: OverlappingFieldsCanMerge
+    uniqueInputFieldNamesRule(ctx),
   ], onAccept: (result) {
     if (result is List<GraphQLError>) {
       // TODO: these are returning error when visiting, not by using reportError
@@ -196,7 +215,7 @@ abstract class NodeWithSelectionSet {
   ///
   const NodeWithSelectionSet();
 
-  Node get node;
+  ExecutableDefinitionNode get node;
 
   const factory NodeWithSelectionSet.operation(OperationDefinitionNode node) =
       _NodeWithSelectionSetOperationDefinition;
