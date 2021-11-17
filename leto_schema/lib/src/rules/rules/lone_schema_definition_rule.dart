7909b833551882722382e102d9934897e8d3cd3c
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-17 01:45:43 +0000

    lone schema definition rule

---
+++ leto_schema/lib/src/rules/rules/lone_schema_definition_rule.dart
@@ -0,0 +1,48 @@
+import '../rules_prelude.dart';
+
+const _loneSchemaDefinitionSpec = ErrorSpec(
+  spec: 'https://spec.graphql.org/draft/#sec-Schema-Extension',
+  code: 'loneSchemaDefinition',
+);
+
+/// Lone Schema definition
+///
+/// A GraphQL document is only valid if it contains only one schema definition.
+///
+/// See https://spec.graphql.org/draft/#sec-Schema-Extension
+Visitor loneSchemaDefinitionRule(
+  SDLValidationCtx context,
+) {
+  final oldSchema = context.schema;
+  final Object? alreadyDefined = oldSchema?.astNode ??
+      oldSchema?.queryType ??
+      oldSchema?.mutationType ??
+      oldSchema?.subscriptionType;
+
+  int schemaDefinitionsCount = 0;
+  final visitor = TypedVisitor();
+  visitor.add<SchemaDefinitionNode>((node) {
+    if (alreadyDefined != null) {
+      context.reportError(
+        GraphQLError(
+          'Cannot define a new schema within a schema extension.',
+          locations: GraphQLErrorLocation.firstFromNodes([node]),
+          extensions: _loneSchemaDefinitionSpec.extensions(),
+        ),
+      );
+      return;
+    }
+
+    if (schemaDefinitionsCount > 0) {
+      context.reportError(
+        GraphQLError(
+          'Must provide only one schema definition.',
+          locations: GraphQLErrorLocation.firstFromNodes([node]),
+          extensions: _loneSchemaDefinitionSpec.extensions(),
+        ),
+      );
+    }
+    schemaDefinitionsCount++;
+  });
+  return visitor;
+}
