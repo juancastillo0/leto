dda0bd92a948ade32ebcd32dfe2c939ffefd84ad
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-17 01:34:15 +0000

    split ValidationCtx into SDLValidationCtx

--- leto_schema/lib/src/rules/validation_context.dart
+++ leto_schema/lib/src/rules/validation_context.dart
@@ -5,19 +5,13 @@ import 'package:leto_schema/src/rules/type_info.dart';
 import 'package:leto_schema/src/rules/typed_visitor.dart';
 import 'package:leto_schema/utilities.dart';
 
-class ValidationCtx {
-  final GraphQLSchema schema;
+class SDLValidationCtx {
+  final GraphQLSchema? schema;
   final DocumentNode document;
-  final TypeInfo typeInfo;
   final void Function(GraphQLError) onError;
 
   late final Map<String, FragmentDefinitionNode> fragmentsMap =
       fragmentsFromDocument(document);
-
-  final Map<ExecutableDefinitionNode, List<VariableUsage>> _variableUsages = {};
-
-  final Map<OperationDefinitionNode, List<VariableUsage>>
-      _recursiveVariableUsages = {};
 
   final Map<OperationDefinitionNode, List<FragmentDefinitionNode>>
       _recursivelyReferencedFragments = {};
@@ -25,57 +19,14 @@ class ValidationCtx {
   final Map<SelectionSetNode, List<FragmentSpreadNode>> _fragmentSpreads = {};
 
   ///
-  ValidationCtx(
-    this.schema,
-    this.document,
-    this.typeInfo, {
+  SDLValidationCtx({
+    required this.schema,
+    required this.document,
     required this.onError,
   });
 
-  final List<GraphQLError> _errors = [];
-
   void reportError(GraphQLError error) {
     onError(error);
-  }
-
-  List<VariableUsage> getVariableUsages(ExecutableDefinitionNode node) {
-    var usages = _variableUsages[node];
-    if (usages == null) {
-      final newUsages = <VariableUsage>[];
-      final typeInfo = TypeInfo(schema);
-
-      final _visitor = TypedVisitor();
-      _visitor.add<VariableDefinitionNode>(
-        (_) => VisitBehavior.skipTree,
-      );
-      _visitor.add<VariableNode>((variable) {
-        newUsages.add(VariableUsage(
-          node: variable,
-          type: typeInfo.getInputType(),
-          defaultValue: typeInfo.getDefaultValue(),
-        ));
-      });
-      node.accept(
-        WithTypeInfoVisitor(typeInfo, visitors: [_visitor]),
-      );
-      usages = newUsages;
-      _variableUsages[node] = usages;
-    }
-    return usages;
-  }
-
-  List<VariableUsage> getRecursiveVariableUsages(
-    OperationDefinitionNode operation,
-  ) {
-    var usages = _recursiveVariableUsages[operation];
-    if (usages == null) {
-      usages = getVariableUsages(operation);
-      for (final frag in getRecursivelyReferencedFragments(operation)) {
-        usages.addAll(getVariableUsages(frag));
-      }
-      _recursiveVariableUsages[operation] = usages;
-    }
-    return usages;
   }
 
   List<FragmentDefinitionNode> getRecursivelyReferencedFragments(
@@ -127,6 +78,68 @@ class ValidationCtx {
       _fragmentSpreads[node] = spreads;
     }
     return spreads;
+  }
+}
+
+class ValidationCtx extends SDLValidationCtx {
+  GraphQLSchema get schema => super.schema!;
+  final TypeInfo typeInfo;
+
+  final Map<ExecutableDefinitionNode, List<VariableUsage>> _variableUsages = {};
+
+  final Map<OperationDefinitionNode, List<VariableUsage>>
+      _recursiveVariableUsages = {};
+
+  ///
+  ValidationCtx(
+    GraphQLSchema schema,
+    DocumentNode document,
+    this.typeInfo, {
+    required void Function(GraphQLError) onError,
+  }) : super(
+          schema: schema,
+          document: document,
+          onError: onError,
+        );
+
+  List<VariableUsage> getVariableUsages(ExecutableDefinitionNode node) {
+    var usages = _variableUsages[node];
+    if (usages == null) {
+      final newUsages = <VariableUsage>[];
+      final typeInfo = TypeInfo(schema);
+
+      final _visitor = TypedVisitor();
+      _visitor.add<VariableDefinitionNode>(
+        (_) => VisitBehavior.skipTree,
+      );
+      _visitor.add<VariableNode>((variable) {
+        newUsages.add(VariableUsage(
+          node: variable,
+          type: typeInfo.getInputType(),
+          defaultValue: typeInfo.getDefaultValue(),
+        ));
+      });
+      node.accept(
+        WithTypeInfoVisitor(typeInfo, visitors: [_visitor]),
+      );
+      usages = newUsages;
+      _variableUsages[node] = usages;
+    }
+    return usages;
+  }
+
+  List<VariableUsage> getRecursiveVariableUsages(
+    OperationDefinitionNode operation,
+  ) {
+    var usages = _recursiveVariableUsages[operation];
+    if (usages == null) {
+      usages = getVariableUsages(operation);
+      for (final frag in getRecursivelyReferencedFragments(operation)) {
+        usages.addAll(getVariableUsages(frag));
+      }
+      _recursiveVariableUsages[operation] = usages;
+    }
+    return usages;
   }
 }
 
