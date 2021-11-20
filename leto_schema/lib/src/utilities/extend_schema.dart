bc41e9cc33517f727436c8175da27766f1e198e1
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-20 02:30:01 +0000

    experimental extend schema

---
+++ leto_schema/lib/src/utilities/extend_schema.dart
@@ -0,0 +1,15 @@
+import 'package:gql/language.dart' show printNode;
+import 'package:leto_schema/leto_schema.dart' show GraphQLSchema, DocumentNode;
+import 'package:leto_schema/utilities.dart' show buildSchema, printSchema;
+import 'package:meta/meta.dart';
+
+/// Extends a [schema] with an SDL [document]
+@experimental
+GraphQLSchema extendSchema(GraphQLSchema schema, DocumentNode document) {
+  final schemaStr = printSchema(schema);
+  final documentStr = printNode(document);
+
+  final combinedStr = '$schemaStr\n\n$documentStr';
+
+  return buildSchema(combinedStr);
+}
