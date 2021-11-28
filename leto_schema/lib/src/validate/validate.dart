4d580bb690e40d03fa9e1305b341848ccc908d25
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-11-28 03:30:19 +0000

    export all validate rules

--- leto_schema/lib/src/validate/validate.dart
+++ leto_schema/lib/src/validate/validate.dart
@@ -1,36 +1,10 @@
 import 'package:gql/ast.dart';
 
 import 'package:leto_schema/leto_schema.dart';
-import 'package:leto_schema/src/validate/rules/implement_interface_once_rule.dart';
-import 'package:leto_schema/src/validate/rules/known_argument_names_rule.dart';
-import 'package:leto_schema/src/validate/rules/known_directive_rule.dart';
-import 'package:leto_schema/src/validate/rules/lone_schema_definition_rule.dart';
-import 'package:leto_schema/src/validate/rules/no_fragment_cycles_rule.dart';
-import 'package:leto_schema/src/validate/rules/no_undefined_variables_rule.dart';
-import 'package:leto_schema/src/validate/rules/no_unused_variables_rule.dart';
-import 'package:leto_schema/src/validate/rules/overlapping_fields_can_be_merged.dart';
-import 'package:leto_schema/src/validate/rules/possible_fragment_spreads_rule.dart';
-import 'package:leto_schema/src/validate/rules/possible_type_extensions_rule.dart';
-import 'package:leto_schema/src/validate/rules/provide_required_arguments_rule.dart';
-import 'package:leto_schema/src/validate/rules/scalar_leafs_rule.dart';
-import 'package:leto_schema/src/validate/rules/single_field_subscription_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_argument_definition_names_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_argument_names_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_directive_names_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_directive_per_location_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_enum_value_names_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_field_definition_names_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_input_field_name_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_operation_types_rule.dart';
-import 'package:leto_schema/src/validate/rules/unique_type_names_rules.dart';
-import 'package:leto_schema/src/validate/rules/unique_variable_names_rule.dart';
-import 'package:leto_schema/src/validate/rules/values_of_correct_type_rule.dart';
-import 'package:leto_schema/src/validate/rules/variables_in_allowed_position_rule.dart';
 import 'package:leto_schema/src/validate/type_info.dart';
 import 'package:leto_schema/src/validate/typed_visitor.dart';
 import 'package:leto_schema/src/validate/validate_schema.dart';
-
-import 'executable_rules.dart';
+import 'package:leto_schema/validate_rules.dart';
 import 'validation_context.dart';
 
 /// Return a visitor that executes document validations for a [ValidationCtx]
@@ -117,7 +91,7 @@ List<GraphQLError> validateDocument(
     onError: (error) {
       if (_errors.length >= maxErrors) {
         _errors.add(
-          const GraphQLError(
+          GraphQLError(
             'Too many validation errors, error limit reached.'
             ' Validation aborted.',
           ),
